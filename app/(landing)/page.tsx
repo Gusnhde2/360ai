c29050f9e8f156fd4a626dd4309d81607c08b0c7
Kemal <lepuzanovickemal@gmail.com>
2023-10-01 22:55:22 +0200

    fix: build issues

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -29,7 +29,75 @@ export default function Home() {
           id="tsparticles"
           init={particlesInit}
           loaded={particlesLoaded}
-          options={config}
+          options={{
+            background: {
+              color: {
+                value: "#fff",
+              },
+            },
+            fpsLimit: 500,
+            interactivity: {
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: "push",
+                },
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                resize: true,
+              },
+              modes: {
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: "#222",
+              },
+              links: {
+                color: "#222",
+                distance: 150,
+                enable: true,
+                opacity: 0.5,
+                width: 1,
+              },
+              move: {
+                direction: "none",
+                enable: true,
+                outModes: {
+                  default: "bounce",
+                },
+                random: false,
+                speed: 2,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 1000,
+                },
+                value: 80,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                value: { min: 1, max: 5 },
+              },
+            },
+            detectRetina: true,
+          }}
         />
       </div>
       <div className="flex flex-col items-center gap-3 mt-64">
