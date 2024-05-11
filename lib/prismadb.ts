89423eca688e10f8eddbf6dfc5207ac5464c83ff
Kemal <lepuzanovickemal@gmail.com>
2024-05-11 16:07:02 +0200

    fix/prisma-client

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -3,8 +3,13 @@ import { PrismaClient } from "@prisma/client";
 declare global {
   var prisma: PrismaClient | undefined;
 }
-
-export const prisma = global.prisma || new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: `${process.env.DATABASE_URL}?statement_cache_size=0`,
+    },
+  },
+});
 if (process.env.NODE_ENV === "development") global.prisma = prisma;
 
 export default prisma;
